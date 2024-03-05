d665f292f94b4eafcadaf168426cb1bd1b27256f
angarciama <angarciama@unal.edu.co>
2024-03-05 13:53:50 -0500

    [Advance] -Se desinstala vue-router -Se genera pagina principal -Se crea componente boton -Se eliminan archivos sobrantes

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Diseño Agronomico de Riego</h1>
+    <ButtonNavigate route="back" buttonText="Volver" />
+    <ButtonNavigate route="back" buttonText="Aspersion" />
+    <ButtonNavigate route="back" buttonText="Salir" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ButtonNavigate from "~/components/buttons/ButtonNavigate.vue";
+</script>
